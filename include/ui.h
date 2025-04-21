02568d7ae6ae3e2134faceb16b4cc84096c68f32
Phong134679 <phonglinhthe5@gmail.com>
2025-04-21 22:20:30 +0700

    Thêm chức năng tạo mật khẩu tự động cho người quản lí

--- include/ui.h
+++ include/ui.h
@@ -6,14 +6,9 @@ void showMenu();
 char getMenuChoice();
 void showRegisterScreen();
 void showLoginScreen();
-<<<<<<< HEAD
-void showRegisterScreenforManager();
 std::string getInput(const std::string& prompt);
 void showMessage(const std::string& message);
-std::string AutoPassword(const std::string& prompt);
-=======
-std::string getInput(const std::string& prompt);
-void showMessage(const std::string& message);
+void showRegisterScreenForMnger();
+void showChangePasswordScreen();
 
->>>>>>> 21cc5e338a896d6f864f983a342a47a25c945da2
 #endif
--- src/ui.cpp
+++ src/ui.cpp
@@ -1,8 +1,9 @@
 #include "../include/ui.h"
 #include <iostream>
 #include <conio.h>
-<<<<<<< HEAD
+
 using namespace std;
+
 void showMenu() {
     cout << "========================\n";
     cout << "TRANSACTION SYSTEM\n";
@@ -11,16 +12,6 @@ void showMenu() {
     cout << "3 REGISTER FOR MANAGER\n";
     cout << "PRESS 1/2/3\n";
     cout << "========================\n";
-=======
-
-void showMenu() {
-    std::cout << "========================\n";
-    std::cout << "TRANSACTION SYSTEM\n";
-    std::cout << "1 LOGIN\n";
-    std::cout << "2 REGISTER\n";
-    std::cout << "PRESS 1/2\n";
-    std::cout << "========================\n";
->>>>>>> 21cc5e338a896d6f864f983a342a47a25c945da2
 }
 
 char getMenuChoice() {
@@ -28,19 +19,15 @@ char getMenuChoice() {
 }
 
 void showRegisterScreen() {
-<<<<<<< HEAD
     cout << ">> REGISTER SYSTEM <<\n";
 }
 
 void showLoginScreen() {
     cout << ">> LOGIN SYSTEM <<\n";
 }
-void showRegisterScreenforManager() {
+
+void showRegisterScreenForMnger() {
 	cout << ">> REGISTER SYSTEM FOR MANAGER <<\n";
-}
-string AutoPassword(const string& prompt) {
-	cout << prompt;
-	return "1";
 }
 string getInput(const string& prompt) {
     cout << prompt;
@@ -51,22 +38,9 @@ string getInput(const string& prompt) {
 
 void showMessage(const string& message) {
     cout << message << "\n";
-=======
-    std::cout << ">> REGISTER SYSTEM <<\n";
 }
 
-void showLoginScreen() {
-    std::cout << ">> LOGIN SYSTEM <<\n";
+void showChangePasswordScreen() {
+	cout << ">> CHANGE YOUR INFORMATION <<\n";
 }
 
-std::string getInput(const std::string& prompt) {
-    std::cout << prompt;
-    std::string input;
-    std::getline(std::cin, input);
-    return input;
-}
-
-void showMessage(const std::string& message) {
-    std::cout << message << "\n";
->>>>>>> 21cc5e338a896d6f864f983a342a47a25c945da2
-}
